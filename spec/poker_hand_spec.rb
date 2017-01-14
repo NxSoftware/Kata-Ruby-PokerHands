d62f20123db458914376d5a326af8af381e5ae33
Steve Wilford <steve@stevewilford.co.uk>
2017-01-14 18:19:23 +0000

    Refactor high card tests

--- spec/poker_hand_spec.rb
+++ spec/poker_hand_spec.rb
@@ -1,31 +1,17 @@
+def expect_high_card(expected_high_card, cards)
+  context "given #{cards}" do
+    it "the high card is #{expected_high_card}" do
+      cards = cards.map { |c| CardParser.parse c }
+      expected_high_card = CardParser.parse expected_high_card
+      hand = PokerHand.new cards
+      expect(hand.high_card).to eq(expected_high_card)
+    end
+  end
+end
+
 describe PokerHand, '#high_card' do
-  
-  context 'given 2H 3H 4H 5H 6H' do
-    it '6H is the high card' do
-      cards = [
-        CardParser.parse('2H'),
-        CardParser.parse('3H'),
-        CardParser.parse('4H'),
-        CardParser.parse('5H'),
-        CardParser.parse('6H'),
-      ]
-      hand = PokerHand.new cards
-      expect(hand.high_card).to eq(cards.last)
-    end
-  end
-  
-  context 'given 6H 2H 3H 4H 5H' do
-    it '6H is the high card' do
-      cards = [
-        CardParser.parse('6H'),
-        CardParser.parse('2H'),
-        CardParser.parse('3H'),
-        CardParser.parse('4H'),
-        CardParser.parse('5H'),
-      ]
-      hand = PokerHand.new cards
-      expect(hand.high_card).to eq(cards.first)
-    end
-  end
+
+  expect_high_card('6H', ['2H', '3H', '4H', '5H', '6H'])
+  expect_high_card('6H', ['6H', '2H', '3H', '4H', '5H'])
   
 end
