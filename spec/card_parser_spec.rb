7d83249c0dc89e95cce38999928ff5fa2ee3ea58
Steve Wilford <steve@stevewilford.co.uk>
2017-01-14 16:09:24 +0000

    Refactor card parsing tests

--- spec/card_parser_spec.rb
+++ spec/card_parser_spec.rb
@@ -1,34 +1,31 @@
 describe CardParser, '#parse' do
   
-  context 'given 2H' do
-    it 'creates a card as 2 of Hearts' do
-      card = CardParser.parse '2H'
-      expect(card.value).to eq(2)
-      expect(card.suit).to eq(Suit::HEARTS)
-    end
-  end
+  inputs = {
+    '2H' => {
+      :value => 2,
+      :suit => Suit::HEARTS
+    },
+    '2D' => {
+      :value => 2,
+      :suit => Suit::DIAMONDS
+    },
+    '2C' => {
+      :value => 2,
+      :suit => Suit::CLUBS
+    },
+    '2S' => {
+      :value => 2,
+      :suit => Suit::SPADES
+    }
+  }
   
-  context 'given 2D' do
-    it 'creates a card as 2 of Diamonds' do
-      card = CardParser.parse '2D'
-      expect(card.value).to eq(2)
-      expect(card.suit).to eq(Suit::DIAMONDS)
-    end
-  end
-  
-  context 'given 2C' do
-    it 'creates a card as 2 of Clubs' do
-      card = CardParser.parse '2C'
-      expect(card.value).to eq(2)
-      expect(card.suit).to eq(Suit::CLUBS)
-    end
-  end
-  
-  context 'given 2S' do
-    it 'creates a card as 2 of Spades' do
-      card = CardParser.parse '2S'
-      expect(card.value).to eq(2)
-      expect(card.suit).to eq(Suit::SPADES)
+  inputs.each do |input, v|
+    context "given #{input}" do
+      it "creates a valid card" do
+        card = CardParser.parse input
+        expect(card.value).to eq(v[:value])
+        expect(card.suit).to eq(v[:suit])
+      end
     end
   end
   
