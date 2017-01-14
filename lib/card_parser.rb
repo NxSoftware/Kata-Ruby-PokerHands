8003980b8d885c773667d901b8dd37dd38fbbcb2
Steve Wilford <steve@stevewilford.co.uk>
2017-01-14 17:44:16 +0000

    Refactor card value conversion

--- lib/card_parser.rb
+++ lib/card_parser.rb
@@ -1,17 +1,21 @@
 class CardParser
   def self.parse(input)
-    value = input[0]
-    case value
-    when '2'..'9' then value = value.to_i
-    when 'T' then value = 10
-    when 'J' then value = 11
-    when 'Q' then value = 12
-    when 'K' then value = 13
-    when 'A' then value = 14
-    else return nil
-    end
-    
+    value = card_value input[0]
     suit = input[1]
-    Card.new(value, suit) if Suit.is_valid?(suit)
+    
+    if value && Suit.is_valid?(suit)
+      return Card.new value, suit
+    end
+  end
+  
+  def self.card_value(input)
+    case input
+    when '2'..'9' then return input.to_i
+    when 'T' then return 10
+    when 'J' then return 11
+    when 'Q' then return 12
+    when 'K' then return 13
+    when 'A' then return 14
+    end
   end
 end
