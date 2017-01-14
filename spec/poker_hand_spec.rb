72cdd6885fad91e2a9afccbf095793b90e5a7346
Steve Wilford <steve@stevewilford.co.uk>
2017-01-14 19:10:06 +0000

    Consistently order the nil test cases

--- spec/poker_hand_spec.rb
+++ spec/poker_hand_spec.rb
@@ -50,17 +50,17 @@ describe PokerHand do
   end
 
   describe '#pair' do
+    expect_pair(nil, [])
+    expect_pair(nil, ['2H', '3C', '4H', '5H', '6H'])
     expect_pair(2, ['2H', '4H', '2C', '5H', '6H'])
     expect_pair(3, ['2H', '3C', '3H', '5H', '6H'])
-    expect_pair(nil, ['2H', '3C', '4H', '5H', '6H'])
-    expect_pair(nil, [])
   end
   
   describe '#two_pair' do
+    expect_two_pair(nil, [])
+    expect_two_pair(nil, ['2H', '3C', '4H', '5H', '6H'])
     expect_two_pair([2, 3], ['2H', '3C', '2S', '4D', '3D'])
     expect_two_pair([9, 10], ['TH', 'TD', '9C', '4C', '9H'])
-    expect_two_pair(nil, ['2H', '3C', '4H', '5H', '6H'])
-    expect_two_pair(nil, [])
   end
   
   describe '#three_of_a_kind' do
