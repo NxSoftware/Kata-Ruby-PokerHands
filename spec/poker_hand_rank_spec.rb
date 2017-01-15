0fe925708fc35346a1fbf939832c719f65599190
Steve Wilford <steve@stevewilford.co.uk>
2017-01-15 18:17:52 +0000

    Refactor hand ranking tests

--- spec/poker_hand_rank_spec.rb
+++ spec/poker_hand_rank_spec.rb
@@ -1,31 +1,28 @@
+def make_hand(cards)
+  PokerHand.new(cards.map do |c|
+    CardParser.parse c
+  end)
+end
+
 describe PokerHand, '#rank' do
   
   context 'given 2H 3D 4S 5C 6H' do
     it 'rank is high card with 6' do
-      hand = PokerHand.new(['2H', '3D', '4S', '5C', '6H'].map do |c|
-        CardParser.parse c
-      end)
-
+      hand = make_hand ['2H', '3D', '4S', '5C', '6H']
       expect(hand.rank).to eq([:high_card, 6])
     end
   end
   
   context 'given 2H 3D 2S 5C 6H' do
     it "rank is pair of 2's" do
-      hand = PokerHand.new(['2H', '3D', '2S', '5C', '6H'].map do |c|
-        CardParser.parse c
-      end)
-
+      hand = make_hand ['2H', '3D', '2S', '5C', '6H']
       expect(hand.rank).to eq([:pair, 2])
     end
   end
   
   context 'given 2H 3D 2S 3C 6H' do
     it "rank is two pair of 2's and 3's" do
-      hand = PokerHand.new(['2H', '3D', '2S', '3C', '6H'].map do |c|
-        CardParser.parse c
-      end)
-
+      hand = make_hand ['2H', '3D', '2S', '3C', '6H']
       expect(hand.rank).to eq([:two_pair, [2, 3]])
     end
   end
