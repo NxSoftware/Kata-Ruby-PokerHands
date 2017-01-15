bc6ece179c0e368bfc289037019b402a4139104e
Steve Wilford <steve@stevewilford.co.uk>
2017-01-15 18:24:04 +0000

    Slightly reduce duplication in rank tests

--- spec/poker_hand_rank_spec.rb
+++ spec/poker_hand_rank_spec.rb
@@ -1,3 +1,8 @@
+def expect_rank(cards, &matcher)
+  hand = make_hand cards
+  expect(hand.rank)
+end
+  
 def make_hand(cards)
   PokerHand.new(cards.map do |c|
     CardParser.parse c
@@ -8,29 +13,29 @@ describe PokerHand, '#rank' do
   
   context 'given 2H 3D 4S 5C 6H' do
     it 'rank is high card with 6' do
-      hand = make_hand ['2H', '3D', '4S', '5C', '6H']
-      expect(hand.rank).to eq([:high_card, 6])
+      cards = ['2H', '3D', '4S', '5C', '6H']
+      expect_rank(cards).to eq([:high_card, 6])
     end
   end
   
   context 'given 2H 3D 2S 5C 6H' do
     it "rank is pair of 2's" do
-      hand = make_hand ['2H', '3D', '2S', '5C', '6H']
-      expect(hand.rank).to eq([:pair, 2])
+      cards = ['2H', '3D', '2S', '5C', '6H']
+      expect_rank(cards).to eq([:pair, 2])
     end
   end
   
   context 'given 2H 3D 2S 3C 6H' do
     it "rank is two pair of 2's and 3's" do
-      hand = make_hand ['2H', '3D', '2S', '3C', '6H']
-      expect(hand.rank).to eq([:two_pair, [2, 3]])
+      cards = ['2H', '3D', '2S', '3C', '6H']
+      expect_rank(cards).to eq([:two_pair, [2, 3]])
     end
   end
   
   context 'given 2H 2D 2S 3C 6H' do
     it "rank is three of a kind of 2's" do
-      hand = make_hand ['2H', '2D', '2S', '3C', '6H']
-      expect(hand.rank).to eq([:three_of_a_kind, 2])
+      cards = ['2H', '2D', '2S', '3C', '6H']
+      expect_rank(cards).to eq([:three_of_a_kind, 2])
     end
   end
   
